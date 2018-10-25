2205c54f4131a7203363a05d15cad46d6777c4cf
LucVerhoevenCarglass <luc.verhoeven@carglass.be>
2018-10-25 14:12:54 +0200

    DigiBookLuc

--- DigiBooky/Domain/Books/Book.cs
+++ DigiBooky/Domain/Books/Book.cs
@@ -7,12 +7,14 @@ namespace Domain.Books
 {
     public class Book
     {
+        public string Id { get; set; }
         public string BookTitle { get; set; }
         public string Isbn { get; set; }
         public Author Author { get; set; }
 
         public Book(string bookTitle, string isbn, Author author)
         {
+            Id = Guid.NewGuid().ToString();
             BookTitle = bookTitle;
             Isbn = isbn;
             Author = author;
