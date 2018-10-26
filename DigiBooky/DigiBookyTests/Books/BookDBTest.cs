c50c85e2bb8b47c06ba4b786083ae64da2a1a43f
larspeelman <lars.peelman@hotmail.com>
2018-10-26 11:21:13 +0200

    test fixed

--- DigiBooky/DigiBookyTests/Books/BookDBTest.cs
+++ DigiBooky/DigiBookyTests/Books/BookDBTest.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Books;
+﻿using Domain.Authors;
+using Domain.Books;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,18 @@ namespace DigiBookyTests.Books
         public void GivenBookDB_WhenGetAllBooks_ThenReturnFullDatabase()
         {
             DBBookRepository dBBooks = new DBBookRepository();
+            Random red = new Random();
+            Author[] authorList = new Author[]
+            {
+                new Author("Jef", "Depaepe"),
+                new Author("Jos", "Schuurlink"),
+                new Author("Guido", "Gazelle")
+            };
+            for (int bookCount = 1; bookCount < 50; bookCount++)
+            {
+                DBBooks.ListofBooks.Add(new Book { BookTitle = $"BookTitle{bookCount}", Author = authorList[red.Next(0, 3)], Isbn = "isbn" + bookCount.ToString() });
+            }
+            
             Assert.Equal(49, dBBooks.GetAllBooks().Count());
         }
 
