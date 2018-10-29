40bfe45a10ebd2a434dee4de5650fca9aab9a027
LucVerhoevenCarglass <luc.verhoeven@carglass.be>
2018-10-29 11:27:47 +0100

    DigiBookLuc

--- DigiBooky/DigiBooky/Startup.cs
+++ DigiBooky/DigiBooky/Startup.cs
@@ -28,7 +28,7 @@ namespace Digibooky_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IBookService, BookService>();
             services.AddSingleton<IRentalService, RentalService>();
             services.AddSingleton<IUserRepository, UserRepository>();
